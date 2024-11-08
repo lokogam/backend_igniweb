74ab253ba83d0a51e2e7d2faf8a53a2743ee4cc5
Duvan <duvangamboa8@gmail.com>
2024-11-08 12:58:36 -0500

    .

--- setup-docker.sh
+++ setup-docker.sh
@@ -6,6 +6,19 @@ if [ ! -f "composer.json" ]; then
     exit 1
 fi
 
+# Verificar si el archivo .env existe; si no, copiar desde .env.example
+if [ ! -f ".env" ]; then
+    echo ".env no encontrado. Creando desde .env.example..."
+    cp .env.example .env
+    echo ".env creado exitosamente."
+else
+    echo ".env ya existe."
+fi
+
+# Generar la clave de la aplicación
+echo "Generando clave de aplicación de Laravel..."
+./vendor/bin/sail artisan key:generate
+
 echo "Iniciando instalación de dependencias con Composer..."
 
 # Ejecutar composer install con Docker
@@ -13,7 +26,8 @@ docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
-    composer install
+    laravelsail/php83-composer:latest \
+    composer install --ignore-platform-reqs
 
 # Verificar si la instalación de composer fue exitosa
 if [ $? -ne 0 ]; then
@@ -21,12 +35,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Instalación de Composer completada. Iniciando Sail..."
-
-# Esperar un momento para asegurar que todos los archivos estén escritos
 sleep 2
 
-# Iniciar Sail
+
+# Ejecutar Composer update y las migraciones con seeds dentro del contenedor
 ./vendor/bin/sail up -d
 
 # Verificar si Sail se inició correctamente
@@ -34,5 +46,28 @@ if [ $? -ne 0 ]; then
     echo "Error: No se pudo iniciar Sail"
     exit 1
 fi
+
+sleep 2
+
+# Actulizacion de  composer
+./vendor/bin/sail composer update
+
+# Verificar si Composer  se actualizó correctamente
+if [ $? -ne 0 ]; then
+    echo "Error: No se pudo  actualizar Composer"
+    exit 1
+fi
+
+sleep 2
+# Ejecutar las migraciones con seeds
+./vendor/bin/sail artisan migrate:fresh --seed
+
+# Verificar si  las migraciones se ejecutaron correctamente
+if [ $? -ne 0 ]; then
+    echo "Error: No se pudo  ejecutar la migración"
+    exit 1
+fi
+
+sleep 2
 
 echo "¡Proceso completado exitosamente!"
